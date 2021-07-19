3e4024b1e5dcc236eddf6c0f7f1cdb43c1b733e5
Lucas <lucsvor@gmail.com>
2021-07-19 20:09:36 -0300

    Add project files.

---
+++ DicasCSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DicasCSharp
+{
+    class Program
+    {
+        private static readonly string _dateAsText = "08 07 2021";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateWithStringAndSubstring());
+            Console.WriteLine(DateWithStringAndSpan());
+        }
+        public static (int day,int month, int year) DateWithStringAndSubstring()
+        {
+            var dayAsText = _dateAsText.Substring(0, 2);
+            var monthAsText = _dateAsText.Substring(3, 2);
+            var yearAsText = _dateAsText.Substring(6);
+            var day = int.Parse(dayAsText);
+            var month = int.Parse(monthAsText);
+            var year = int.Parse(yearAsText);
+            return (day, month, year);
+        }
+        public static (int day, int month, int year) DateWithStringAndSpan()
+        {
+            ReadOnlySpan<char> dateAsSpan = _dateAsText.AsSpan();
+            var dayAsText = dateAsSpan.Slice(0, 2);
+            var monthAsText = dateAsSpan.Slice(3, 2);
+            var yearAsText = dateAsSpan.Slice(6);
+            var day = int.Parse(dayAsText.ToString());
+            var month = int.Parse(monthAsText.ToString());
+            var year = int.Parse(yearAsText.ToString());
+            return (day, month, year);
+        }
+    }
+}
